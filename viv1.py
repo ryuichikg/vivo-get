90893bdf3fa710acf9a675188b2198a45014d773
ryuichikg <87591277+ryuichikg@users.noreply.github.com>
2024-06-24 23:14:31 +0900

    Add files via upload

--- viv1.py
+++ viv1.py
@@ -13,28 +13,58 @@ st.write('テスト版')
 st.text(datetime.date.today())
 
 """
-## 今週のロング銘柄・ショート銘柄
+## 今週のロング銘柄ショート銘柄
 """
-df1 = pd.read_csv('https://github.com/ryuichikg/vivo-get/blob/main/KS211.csv', encoding='utf-8', index_col=0)
-df3 = pd.read_csv('https://github.com/ryuichikg/vivo-get/blob/main/KS212.csv', encoding='utf-8', index_col=0)
-df2 = pd.read_csv('https://github.com/ryuichikg/vivo-get/blob/main/KS22.csv', encoding='utf-8', index_col=0)
-#st.dataframe(df)
+df1 = pd.DataFrame({
+    '買い銘柄':['三菱重工','村田製作所','りそな','トヨタ自動車','日本電信電話'],
+    '現在価格':['1613.5','3345','1012.5','3150','149.4'],
+    '目標価格':['1645','3400','1050','3350','155']
+})
 df1
-df3
-st.write('１．日経プライム市場で大型中型かつ低予算で組める銘柄を選出')
+
+df2 = pd.DataFrame({
+    '売り銘柄':['住友不動産','パナソニック','日本製鉄','SUBARU','浜松ホトニクス'],
+    '現在価格':['5190','1303.5','3343','3354','4471'],
+    '目標価格':['5100','1260','3250','3330','4450']
+})
+df2
+
+#st.dataframe(df)
+st.write('１．日経プライム市場で取引量のある主要銘柄を選定')
 st.write('２．各々３銘柄を売買い保有')
 st.write('３．一定の利益または損失がでたら一括決済')
 
 """
 ##  ＮＴ倍率
 """
-df2
+df3 = pd.DataFrame({
+    '指数':['日経平均225'],
+    '現在価格':['38804.65'],
+    '上値抵抗':['39500'],
+    '下値支持':['38000']
+})
+df3
+
+df4 = pd.DataFrame({
+    '指数':['TOPIX'],
+    '現在価格':['2740.19'],
+    '上値抵抗':['2800'],
+    '下値支持':['2700']
+})
+df4
+
+data = pd.DataFrame({
+    'NT':['上値抵抗','現在','下値支持'],
+    'レシオ':['14.250','14.161','13.850']
+    })
+data
+
 st.write('もみ合い続く :lightning:')
 """
-## 日経平均とTOPIXグラフ
+## ＮＴレシオグラフ
 """
-submit = st.button('会員登録はこちら')
+submit = st.button('押して会員登録確定 　98万/月')
 if submit == True:
-    st.bar_chart(df2)
+    st.bar_chart(data.set_index('NT'))
     # plt.bar(df)
 st.write("投資のご判断は自己責任で")
