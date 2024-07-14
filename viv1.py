4e1b2a47fe9863b80328b7619cdf5d2ef71954d2
ryuichikg <87591277+ryuichikg@users.noreply.github.com>
2024-07-14 22:38:31 +0900

    Add files via upload

--- viv1.py
+++ viv1.py
@@ -9,25 +9,29 @@ from PIL import Image
 st.title('ロング・ショート戦略')
 #color = st.color_picker('summer sales contest', '#87ceeb')
 
+txt0 = '<p style="color:red;">※7月14日に米国にて暗殺未遂事件発生のため要注意</p>'
+st.write(txt0, unsafe_allow_html=True)
+
+
 st.write('テスト版')
 
-st.text('更新日7月7日')
+st.text('更新日7月14日')
 st.text(datetime.date.today())
 
 """
 ## 今週のロング銘柄ショート銘柄
 """
 df1 = pd.DataFrame({
-    '買い銘柄':['アニコムHD','三井住友建設','セブン銀行','東レ','日立造船'],
-    '現在価格':['680','403','274.4','746.8','1094'],
-    '目標価格':['710','420','290','785','1180']
+    '買い銘柄':['インフォマート','日東精工','東海東京','シャープ','鎌倉新書'],
+    '現在価格':['337','641','580','968.8','438'],
+    '目標価格':['370','700','600','1000','515']
 })
 df1
 
 df2 = pd.DataFrame({
-    '売り銘柄':['杏林製薬','ラウンドワン','旭化成','オンワードHLD','レオパレス２１'],
-    '現在価格':['1683','801','1008.5','580','517'],
-    '目標価格':['1650','750','980','530','490']
+    '売り銘柄':['ダブルスコープ','三協立山','三菱製紙','ユニチカ','ENEOS'],
+    '現在価格':['517','780','741','301','813.7'],
+    '目標価格':['500','740','710','280','800']
 })
 df2
 
@@ -35,9 +39,9 @@ df2
 st.write('１．プライム市場で取引量のある主要銘柄を選定')
 st.write('２．各々３銘柄を売買い保有')
 st.write('３．一定の利益または損失がでたら一括決済')
-txt1 = '<p style="color:red;">先週の実績 買い推奨５銘柄計 +13.48%</p>'
+txt1 = '<p style="color:red;">先週の実績 買い推奨５銘柄計 +3.01%</p>'
 st.write(txt1, unsafe_allow_html=True)
-txt2 = '<p style="color:blue;">先週の実績 売り推奨５銘柄計 -32.88%</p>'
+txt2 = '<p style="color:blue;">先週の実績 売り推奨５銘柄計 -14.82%</p>'
 st.write(txt2, unsafe_allow_html=True)
 
 """
@@ -45,27 +49,27 @@ st.write(txt2, unsafe_allow_html=True)
 """
 df3 = pd.DataFrame({
     '指数':['日経平均225'],
-    '現在価格':['41010'],
-    '上値抵抗':['41500'],
-    '下値支持':['40750']
+    '現在価格':['41190'],
+    '上値抵抗':['42500'],
+    '下値支持':['39450']
 })
 df3
 
 df4 = pd.DataFrame({
     '指数':['TOPIX'],
-    '現在価格':['2884.5'],
-    '上値抵抗':['2910'],
-    '下値支持':['2850']
+    '現在価格':['2894.56'],
+    '上値抵抗':['2950'],
+    '下値支持':['2830']
 })
 df4
 
 data = pd.DataFrame({
     'NT':['上値抵抗','現在','下値支持'],
-    'レシオ':['14.250','14.217','13.850']
+    'レシオ':['14.45','14.23','14.20']
     })
 data
 
-st.write('もみ合い続く。14.25上抜けできるか。 :lightning:')
+st.write('14.20上抜け。上昇できるか。 :+1:')
 """
 ## ＮＴレシオグラフ
 """
@@ -79,16 +83,16 @@ image1 = Image.open('nt01.jpg')
 st.image(image1, caption='NTチャート',use_column_width=True)
 
 """
-## 225/DOW/XAUチャート 1時間足
+## 225:15足/1H足 XAUチャート 
 """
 Image2 = Image.open('bt1.jpg')
-st.image(Image2,caption='225チャート',use_column_width=True)
+st.image(Image2,caption='225チャート15M足',use_column_width=True)
 
 Image3 = Image.open('bt2.jpg')
-st.image(Image3,caption='DOWチャート',use_column_width=True)
+st.image(Image3,caption='225チャート1H足',use_column_width=True)
 
 Image4 = Image.open('bt3.jpg')
-st.image(Image4,caption='XAUチャート',use_column_width=True)
+st.image(Image4,caption='XAUチャート日足',use_column_width=True)
 
 """
 ## 先物・オプション：シュミレーション
@@ -97,6 +101,16 @@ st.image(Image4,caption='XAUチャート',use_column_width=True)
 ### 日経平均先物7月限マイクロ1枚買い41000
 ### 日経平均ミニオプション7月限 39000プット買い10円
 ### 7月限(SQ7月12日)での短期決戦。結果は？
+
+### 先物 SQ 41531  +5310円
+### オプション -1000円
+
+## 今週のオプション戦略はロングストラドル
+### 7月3週SQ
+###・権利行使価格41500
+###・コール@190買い  プット@500買い
+###・合計69000円
+
 """
 
 
